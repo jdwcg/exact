846af16d2fa9eb8d8913a94ad8c1fe491de03654
jdwcg <bigcloudjo@gmail.com>
2021-12-12 23:12:50 +0900

    11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let [글제목,글제목변경] = useState(['남자코트추천','강남우동맛집']);
+  let [따봉, 따봉변경] = useState(0);
+  
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = "여자코트 추천";
+    글제목변경(newArray);
+  }
+
+  let title = 'Title';
   return (
-    <div className="contain">
-    <h2>WEB</h2>
-    <p>Number: {this.props.initNumber}</p>
+    <div className="App">
+      <div className="">
+        <h1>{title}</h1>
+        <button onClick={ 제목바꾸기 }>버튼</button>
+        <ul>
+          <li>{글제목[0]} <span onClick={()=>{따봉변경(따봉 + 1)}}>따봉</span>{따봉}</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+
+      <Modal />
+
     </div>
-
-
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+    <h2>제목</h2>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
 }
 
 export default App;
